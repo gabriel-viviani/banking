c04dca4d2ed74e2d83f7cac51f6881b65057d391
gabriel-viviani <gabrielviviani98@hotmail.com>
2021-12-12 11:07:55 -0300

    feat: Added basic configuration and project structure

---
+++ src/build_app.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from .config import get_database_url
+
+
+engine = create_engine(
+    get_database_url(), connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+application = FastAPI()
